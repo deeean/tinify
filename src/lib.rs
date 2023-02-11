90e95b83d13e8b2cd8068dad4e7aff223aebd165
dean <margintop3498@icloud.com>
2023-02-11 11:30:30 +0900

    Support jpg compression

---
+++ src/lib.rs
@@ -0,0 +1,148 @@
+use exoquant::{Color, convert_to_indexed, ditherer, optimizer};
+use image::GenericImageView;
+use imghdr::Type;
+use lodepng::RGBA;
+
+#[derive(Debug)]
+pub enum Error {
+    IoError(std::io::Error),
+    QualityOutOfRange,
+    UnexpectedImageType,
+    UnsupportedImageType(Type),
+    ImageError(image::ImageError),
+    LodepngError(lodepng::Error),
+    MozjpegError,
+}
+
+pub fn tinify<P>(path: P, quality: i32) -> Result<Vec<u8>, Error> where P: AsRef<std::path::Path> {
+    if (quality < 0) || (quality > 100) {
+        return Err(Error::QualityOutOfRange);
+    }
+
+    let buf = match std::fs::read(path) {
+        Ok(buf) => buf,
+        Err(e) => return Err(Error::IoError(e)),
+    };
+
+    let format = match imghdr::from_bytes(&buf) {
+        None => return Err(Error::UnexpectedImageType),
+        Some(format) => match format {
+            Type::Png | Type::Jpeg => format,
+            _ => return Err(Error::UnsupportedImageType(format)),
+        },
+    };
+
+    let img = match format {
+        Type::Png => image::load_from_memory_with_format(&buf, image::ImageFormat::Png),
+        Type::Jpeg => image::load_from_memory_with_format(&buf, image::ImageFormat::Jpeg),
+        _ => unreachable!(),
+    };
+
+    let img = match img {
+        Ok(img) => img,
+        Err(e) => return Err(Error::ImageError(e)),
+    };
+
+    let num_colors = (quality as f64 / 100.0 * 256.0).floor() as usize;
+
+    let width = img.width() as usize;
+    let height = img.height() as usize;
+    let buffer = img
+        .pixels()
+        .map(|(_, _, c)| Color::new(c.0[0], c.0[1], c.0[2], c.0[3]))
+        .collect::<Vec<Color>>();
+
+    let (palette, indexed_data) = convert_to_indexed(
+        &buffer,
+        width as usize,
+        num_colors,
+        &optimizer::WeightedKMeans,
+        &ditherer::FloydSteinberg::checkered(),
+    );
+
+    match format {
+        Type::Png => {
+            let buffer = indexed_data
+                .iter()
+                .map(|x| {
+                    let color = palette[*x as usize];
+                    [color.r, color.g, color.b, color.a]
+                })
+                .flatten()
+                .collect::<Vec<_>>();
+
+            match lodepng::encode_memory(&buffer, width, height, lodepng::ColorType::RGBA, 8) {
+                Ok(x) => Ok(x),
+                Err(e) => Err(Error::LodepngError(e)),
+            }
+        },
+        Type::Jpeg => {
+            let buffer = indexed_data
+                .iter()
+                .map(|x| {
+                    let color = palette[*x as usize];
+                    [color.r, color.g, color.b]
+                })
+                .flatten()
+                .collect::<Vec<_>>();
+
+            let mut comp = mozjpeg::Compress::new(mozjpeg::ColorSpace::JCS_RGB);
+
+            comp.set_size(width, height);
+            comp.set_mem_dest();
+            comp.set_quality(90.0);
+            comp.start_compress();
+            comp.write_scanlines(&buffer);
+            comp.finish_compress();
+
+            match comp.data_to_vec() {
+                Ok(x) => Ok(x),
+                Err(_) => Err(Error::MozjpegError),
+            }
+        }
+        _ => return Err(Error::UnsupportedImageType(format)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Write;
+    use super::*;
+
+    #[test]
+    fn test_tinify() {
+        let entries = std::fs::read_dir("./testdata")
+            .unwrap()
+            .map(|x| x.unwrap().path())
+            .filter(|x| {
+                let ext = match x.extension() {
+                    Some(x) => match x.to_str() {
+                        Some(x) => x,
+                        None => return false,
+                    },
+                    None => return false,
+                };
+
+                ext == "png" || ext == "jpg"
+            })
+            .collect::<Vec<_>>();
+
+        entries
+            .iter()
+            .map(|x| {
+                println!("Compressing {}...", x.to_str().unwrap());
+
+                let file_name = x.file_stem().unwrap().to_str().unwrap();
+                let ext = x.extension().unwrap().to_str().unwrap();
+
+                match tinify(x, 70) {
+                    Ok(buf) => {
+                        let mut file = std::fs::File::create(format!("./dist/{}_tinify.{}", file_name, ext)).unwrap();
+                        file.write_all(&buf).unwrap();
+                    }
+                    Err(e) => panic!("{:?}", e),
+                }
+            })
+            .collect::<Vec<_>>();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,12 @@
-use std::time::Instant;
-use exoquant::{Color, convert_to_indexed, ditherer, optimizer};
-use lodepng::RGBA;
-use rayon::prelude::*;
+use std::io::Write;
+use tinify::tinify;
 
 fn main() {
-    let quality = 70;
-    let now = Instant::now();
-    let img = lodepng::decode32_file("./testdata/lenna.png").unwrap();
-
-    let num_colors = (quality as f64 / 100.0 * 256.0).floor() as usize;
-
-    let buffer = img.buffer
-        .par_iter()
-        .map(|c| Color::new(c.r, c.g, c.b, c.a))
-        .collect::<Vec<Color>>();
-
-    let width = img.width;
-    let height = img.height;
-
-    let (palette, indexed_data) = convert_to_indexed(
-        &buffer,
-        width as usize,
-        num_colors,
-        &optimizer::WeightedKMeans,
-        &ditherer::FloydSteinberg::checkered(),
-    );
-
-    let palette = palette
-        .par_iter()
-        .map(|x| RGBA::new(x.r, x.g, x.b, x.a))
-        .collect::<Vec<_>>();
-
-    let mut state = lodepng::Encoder::new();
-    state.set_palette(&palette).expect("TODO: panic message");
-    state.encode_file("./testdata/compressed/lenna_tinify.png", &indexed_data, width as usize, height as usize).unwrap();
-
-    println!("Time taken: {}ms", now.elapsed().as_millis());
+    match tinify("./testdata/peppers.jpg", 90) {
+        Ok(buf) => {
+            let mut file = std::fs::File::create("./res.jpg").unwrap();
+            file.write_all(&buf).unwrap();
+        }
+        Err(e) => panic!("{:?}", e),
+    }
 }
